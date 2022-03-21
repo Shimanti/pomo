10af682fa9f407b6ba9b50fe599c7421e743e24f
Shimanti <shimanti@gmail.com>
2022-03-21 12:45:32 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const pomo = Date.now() + 5 * 1000
+
+const countdown = () => {
+  const current = Date.now()
+  const totalSeconds = (pomo - current) / 1000
+
+  if(totalSeconds>=0) {
+    const mins = Math.floor(totalSeconds / 60)
+    const seconds = Math.round(totalSeconds % 60)
+
+    document.querySelector('#mins').innerHTML = format(mins)
+    document.querySelector('#seconds').innerHTML = format(seconds)
+  
+  } else if(totalSeconds<=0) {
+    document.body.innerHTML = ""   
+    playConfetti()
+  }
+}
+
+const format = (time) => time < 10 ? `0${time}` : time
+
+const playConfetti = () => {
+  const jsConfetti = new JSConfetti()
+  jsConfetti.addConfetti()
+}
+
+countdown()
+setInterval(countdown, 1000)
